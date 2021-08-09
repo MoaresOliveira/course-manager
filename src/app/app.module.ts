195aa47dd4cbd50c01787df98177a3b9e6f871ca
MoaresOliveira <moaresoliveira@gmail.com>
2021-08-09 14:52:06 -0300

    Course Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFound404Component } from './NotFound 404/NotFound404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     NotFound404Component,
-    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    CourseModule,
     RouterModule.forRoot([
       {
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full',
-      },
-      {
-        path: 'courses',
-        component: CourseListComponent,
-      },
-      {
-        path: 'courses/info/:id',
-        component: CourseInfoComponent,
       },
       {
         path: '**',
